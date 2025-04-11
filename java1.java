a9941ac50ff5b93e7fe927cd2496e8aa2e941109
lakshmi22-2007 <blakshminarayanan.it2024@citchennai.net>
2025-04-11 13:23:59 +0530

    Create java1.java

---
+++ java1.java
@@ -0,0 +1,7 @@
+public class java1 {
+  public static void main(String[] args)
+  {
+      float radius = 5.8f; 
+      float java1 = 3.1415f * radius * radius;  
+      System.out.println(java1);  
+  }
